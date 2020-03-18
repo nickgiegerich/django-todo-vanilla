9c9533532615a39ef113ba47d4913eccd0cba051
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2020-03-18 08:01:33 -0700

    added modal for update

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,11 +2,22 @@ from django import forms
 from django.forms import ModelForm
 
 from .models import *
+from bootstrap_modal_forms.forms import BSModalForm
 
 
 class TaskForm(forms.ModelForm):
     title = forms.CharField(widget = forms.TextInput(attrs = {'placeholder':'Add new task...'}))
 
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+class EditTaskForm(BSModalForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+class DeleteTaskForm(BSModalForm):
     class Meta:
         model = Task
         fields = '__all__'
--- todo/settings.py
+++ todo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'tasks',
+    'bootstrap_modal_forms',
 ]
 
 MIDDLEWARE = [
